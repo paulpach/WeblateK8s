64b36d5b15bc0c930cbd0a9b5f893ebb00ef041a
Paul Pacheco <paulpach@gmail.com>
2018-08-08 01:30:06 -0500

    credentials now come from a secret

---
+++ secret.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "Enter name: " username
+read -s -p "Enter password: " password
+read -p "Enter email: " email
+
+kubectl create secret generic admin --from-literal=EMAIL="$email" --from-literal=NAME="$username" --from-literal=PASSWORD="$password"
