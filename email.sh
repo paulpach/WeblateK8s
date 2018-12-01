0cbc2be912d2ccfd387788ad69590465eb8ff51b
Paul Pacheco <paulpach@gmail.com>
2018-12-01 08:51:15 -0600

    Improve configuration

--- email.sh
+++ email.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-read -p "Enter smtp server: " host
-read -p "Enter username: " username
+read -e -p "Enter smtp server (smtp.sendgrid.net): " host
+host=${host:-smtp.sendgrid.net}
+read -e -p "Enter port (587): " port
+read -e -p "Server email: " serveremail
+port=${port:-587}
+read -e -p "Enter username: " username
 read -s -p "Enter password: " password
+echo
+
+echo "host $host"
+echo "port $port"
+echo "username $username"
+echo "server email $serveremail"
+
+kubectl delete secret email
 
 kubectl create secret generic email \
     --from-literal=WEBLATE_EMAIL_USER="$username" \
     --from-literal=WEBLATE_EMAIL_HOST="$host" \
+    --from-literal=WEBLATE_EMAIL_PORT="$port" \
     --from-literal=WEBLATE_EMAIL_PASSWORD="$password" \
+    --from-literal=WEBLATE_SERVER_EMAIL="$serveremail" \
+    --from-literal=WEBLATE_DEFAULT_FROM_EMAIL="$serveremail"
     
